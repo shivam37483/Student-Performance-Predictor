6fecdfb054411a9c84a02b8c7de975b40ed60035
shivam37483 <vatshivam49888@gmail.com>
2023-10-27 12:46:17 +0530

    Correction in model_trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,7 +21,7 @@ from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    trained_model_file_path: str = os.path.join('artifacts','model.pkl')
 
 class ModelTrainer:
     def __init__(self):
@@ -51,11 +51,12 @@ class ModelTrainer:
             model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                                 models=models)
 
-            best_model_score = max(sorted(model_report.values))       #get best model score
+            best_model_score = max(sorted(model_report.values()))       #get best model score
 
             best_model_name = list(model_report.keys())[
-                list(model_report.values().index(best_model_score))
+                list(model_report.values()).index(best_model_score)
             ]  
+
 
             best_model = models[best_model_name]
 
@@ -63,7 +64,6 @@ class ModelTrainer:
                 raise CustomException("No best model found")
 
             logging.info(f"Best found model on both training and testing dataset")
-            
                              
             save_object(                       #saving file path
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -77,4 +77,4 @@ class ModelTrainer:
 
 
         except Exception as e:
-            CustomException(e,sys)
+            raise CustomException(e,sys)
